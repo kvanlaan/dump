68176b4edc1eaff4a9d3e484912abf6ac49eefea
Katrina Van Laan <kdvanlaan@gmail.com>
2019-09-21 22:58:13 -0700

    Display more images for landfill component

--- src/app/landfill/landfill.component.ts
+++ src/app/landfill/landfill.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-landfill',
   templateUrl: './landfill.component.html',
   styleUrls: ['./landfill.component.css']
 })
-export class LandfillComponent implements OnInit {
+export class LandfillComponent  implements OnInit {
   @Input() list: any;
   @Input() query: string;
-  constructor() { }
-  ngOnInit() {
+  listingImages = [];
+  http;
+  constructor(http: HttpClient) {
+    this.http = http;
   }
+async ngOnInit() {
+//   for (const  listing of this.list) {
+//     let imgSrc = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location='
+// + listing.Latitude + ',' + listing.Longitude + '&heading=151.78&pitch=-0.76&key=AIzaSyA_JKefOG1BM8rJZoLt7p6OCEdnwPtjiWM'
+// this.getImage(imgSrc).subscribe( res => {
+// res.onload(blob => {
+//   if ((blob.status === 'Zero_Results') &&  (listing['Address'])) {
+//     imgSrc  = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location='
+//      + listing['Site Name'] + ',' + listing.County + '&heading=151.78&pitch=-0.76&key=AIzaSyA_JKefOG1BM8rJZoLt7p6OCEdnwPtjiWM';
+//   }
+//   this.listingImages.push(imgSrc)
+// }
+
+//   )});
+// }
+
+}
+  getImage(latLongString) {
+
+    // return  this.http.get(latLongString, { responseType: 'blob' });
+
+}
 }
