f1257bb40aee4ab7ca8589008b8840b5f7a247b4
Katrina Van Laan <kdvanlaan@gmail.com>
2019-09-22 22:41:45 -0700

    Change to more relaxing green, add query highlighting in results

--- src/app/recycling/recycling.component.ts
+++ src/app/recycling/recycling.component.ts
@@ -14,24 +14,15 @@ export class RecyclingComponent implements OnInit {
   @Output() openOrderPickupModal: EventEmitter<OrderPickupEvent> = new EventEmitter();
   constructor(private http: HttpClient) { }
   ngOnInit() {
-    for (const listObj of this.list) {
-      // tslint:disable-next-line:max-line-length
-      //     const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-{{listObj.Lat}},{{listObj.Lon}}&radius=500&key=AIzaSyDwYoJzlRs_9_TAzOaMDwqWlhtZm8a0GjM'
-      //     let searchRes = null;
-      //     this.http.get(url)
-      //       .map(res => res.json())
-      //       .subscribe(
-      //       data => searchRes = data,
-      //       err => console.log(err),
-      //       () => {
-      //         if (searchRes) {
-      //           console.log('searchRes', searchRes);
-
-      //   }
-      // })
-    }
   }
   prepareOrderPickupEvent(address: string) {
     this.openOrderPickupModal.emit({ dropOffLocationAddress: address, item: this.query } as OrderPickupEvent);
+  }
+  buildMaterialsString(materialsString) {
+    if((materialsString.indexOf(this.query) > -1)) {
+      materialsString = materialsString.replace(this.query, '<strong class="bold light-red italic">' + this.query + '</strong>')
+    }
+    return materialsString;
+
   }
 }
